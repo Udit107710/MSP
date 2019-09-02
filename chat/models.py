9ba688d9239003a7d4d71f25ff6e07e79ada6d8a
Udit <500062348@stu.upes.ac.in>
2019-09-02 15:00:59 +0530

    initial commit

---
+++ chat/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here
+
+
+# class ChatMessage(models):
+#     sender = models.CharField(max_length=50)
+#     message = models.CharField(max_length=200)
