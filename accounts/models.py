bcbe77c8d5f16daad7910c72febfb57da5fe1b67
Udit <500062348@stu.upes.ac.in>
2019-09-15 14:42:42 +0530

    model changed

--- accounts/models.py
+++ accounts/models.py
@@ -18,32 +18,36 @@ class PossiblePhoneNumberField(PhoneNumberField):
 
 
 class Teacher(models.Model):
+    USER_TYPE_CHOICES = [
+        ("Student", "0"),
+        ("HOD", "1"),
+        ("AC", "2"),
+        ("Professor", "3")]
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="teachers")
     avatar = VersatileImageField(upload_to="user-avatars", blank=True, null=True)
     field_of_study = models.CharField(max_length=200)
     slots_occupied = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(0)], default=0)
     phone = PhoneNumberField(null=True)
     department = models.CharField(max_length=200)
-    is_student = models.BooleanField(default=False)
-    is_ac = models.BooleanField(default=False)
-    is_professor = models.BooleanField(default=False)
-    is_hod = models.BooleanField(default=False)
+    type_of_user = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default="3")
 
     class Meta:
         permissions = [('manage_users', 'Can manage users'), ('manage_staff', "Can manage staff")]
 
 
 class Student(models.Model):
+    USER_TYPE_CHOICES = [
+        ("Student", "0"),
+        ("HOD", "1"),
+        ("AC", "2"),
+        ("Professor", "3")]
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="students")
     enrollment_number = models.CharField(max_length=10)
     sap_id = models.CharField(max_length=10)
     program = models.CharField(max_length=100)
     semester = models.IntegerField(validators=[MaxValueValidator(12), MinValueValidator(1)])
     cgpa = models.FloatField(blank=True)
-    is_student = models.BooleanField(default=True)
-    is_ac = models.BooleanField(default=False)
-    is_professor = models.BooleanField(default=False)
-    is_hod = models.BooleanField(default=False)
+    type_of_user = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default="3")
 
     class Meta:
         permissions = [('manage_users', 'Can manage users')]
