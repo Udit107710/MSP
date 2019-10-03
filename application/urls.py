9f97b85047f7377f79d21760bc1429e22ad780e4
Udit <500062348@stu.upes.ac.in>
2019-10-03 14:27:39 +0530

    bug fix

--- application/urls.py
+++ application/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ProposeProject, StudentProposalViewSet, DetailProposalViewSet, GetExcel, MentorProposalList
+from .views import ProposeProject, StudentProposalViewSet, DetailProposalViewSet, GetExcel, MentorProposalList, MentorProposalViewSet
 
-# mentor_proposal_list = MentorProposalViewSet.as_view({
-#     'get': 'list'
-# })
+mentor_proposal_list = MentorProposalViewSet.as_view({
+    'get': 'list'
+})
 
 student_proposal_list = StudentProposalViewSet.as_view({
     'get': 'list'
@@ -16,7 +16,7 @@ proposal_detail = DetailProposalViewSet.as_view({
 
 urlpatterns = [
     path(r"propose/", ProposeProject.as_view(), name="propose-project"),
-    path(r"proposal/mentor/<str:mentor__user__username>", MentorProposalList.as_view(), name="mentor-proposal-list"),
+    path(r"proposal/mentor/<str:mentor__user__username>", mentor_proposal_list, name="mentor-proposal-list"),
     path(r"proposal/student/<str:members>", student_proposal_list, name="student-proposal-list"),
     path(r"proposal/detail/<str:pk>", proposal_detail, name="proposal-detail"),
     path(r"proposals/excel/<str:username>", GetExcel.as_view, name="proposals-excel")
